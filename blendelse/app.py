17691a601a0f06c806e82a761a3ab9fc2c6b6471
amarjithnv <amarjithon@gmail.com>
2025-10-08 21:23:06 +0530

    Update app.py

--- blendelse/app.py
+++ blendelse/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import random
+import os
 
 app = Flask(__name__)
 
@@ -46,4 +47,6 @@ def blend():
     return render_template('blend.html')
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 4000))
+    app.run(host="0.0.0.0", port=port)
+
