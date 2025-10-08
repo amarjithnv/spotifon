3cd824ebba86978330c9adf561ebe92df4d70deb
amarjithnv <amarjithon@gmail.com>
2025-10-08 21:58:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def blend():
     return render_template('blend.html')
     
 if __name__ == '__main__':
-    if __name__ == "__main__":
     port = int(os.environ.get("PORT", 4000))
     app.run(host="0.0.0.0", port=port)
+
 
